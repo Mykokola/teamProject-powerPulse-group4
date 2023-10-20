cfbd29ccfb14402333b5c644ca823f831116dd08
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-10-19 19:48:08 -0500

    add swagger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -12,9 +15,8 @@ app.use(cors());
 app.use(passport.initialize());
 app.use(cookieParser());
 app.use(express.json());
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/',foodRouter)
-
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
