01aa7504d5098ca0406e382f99ea452fb6132b96
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-10-23 13:09:36 -0500

    add save-exercise

--- src/controllers/diary.js
+++ src/controllers/diary.js
@@ -1,6 +1,7 @@
 const createError = require("../utils/createError");
 const diaryService = require("../services/diary");
 const productService = require("../services/products")
+const exerciseService = require("../services/exercises")
 const ERROR_TYPES = require("../constants/ERROR_CODES");
 const validateSchema = require("../models/joi/diary");
 const { nanoid } = require("nanoid");
@@ -46,6 +47,13 @@ const saveExercise = async (req, res, next) => {
     if (!exercise.date) {
       let date = new Date().toISOString().split("T")[0];
       exercise.date = date;
+    }
+    const exerciseFromBd = await exerciseService.getExerciseById({_id:exercise.exercise})
+    if(!exerciseFromBd){
+      const error = createError(ERROR_TYPES.NOT_FOUND,{
+        message:"exercise is not a found"
+      })
+      throw error
     }
     await diaryService.addInDiaryExercise(_id, { ...exercise, id: nanoid() });
     res.status(200).json({ message: "exercise was add" });
