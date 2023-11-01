3f0cb3d81afbfbdddf67f5dec73a42cc91ffafdf
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-11-01 17:25:07 +0200

    видалив непотрібні раути по продуктах

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,11 +4,7 @@ const router = express.Router();
 const ctrl = require("../controllers/products");
 const authMiddleware = require("../middlewears/authMiddleware");
 
-//PRIVATE
 router.get("/", authMiddleware, ctrl.getAllProducts);
 router.get("/categories", authMiddleware, ctrl.getAllCategories);
-// router.get("/filter-product",authMiddleware, ctrl.filterProduct)
-router.get("/available-blood-products/:type", authMiddleware, ctrl.getAvailableProducts); //:true or false
 
-//PRIVATE
 module.exports = router;
