062568949ff46e0194b3bf208257bf7eb67e062f
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-11-01 18:42:06 -0500

    Update diary.js

--- src/models/mongoose/diary.js
+++ src/models/mongoose/diary.js
@@ -1,15 +1,92 @@
 const {Schema,model} = require('mongoose')
+
+const consumedProductSchema = new Schema({
+    calories:{
+        type:Number,
+        required:true
+    },
+    category:{ 
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    groupBloodNotAllowed:{
+        type:Boolean,
+        required:true
+    },
+    burnedCalories:{
+        type:Number,
+        required:true
+    },
+    amount:{
+        type:Number,
+        required:true
+    },
+    date:{
+        type:String,
+        required:true
+    },
+    id:{
+        type:String,
+        required:true
+    }
+
+})
+
+const exerciseDoneSchema = new Schema({
+    bodyPart:{
+        type:String,
+        required:true
+    },
+    equipment:{
+        type:String,
+        required:true
+    },
+    gifUrl:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    target:{
+        type:String,
+        required:true
+    },
+    time:{
+        type:Number,
+        required:true
+    },
+    burnedCalories:{
+        type:Number,
+        required:true
+    },
+    date:{
+        type:String,
+        required:true
+    },
+    id:{
+        type:String,
+        required:true
+    }
+})
+
+
 const dairySchema = new Schema({
     clientId:{
         type:String,
         require:[true,'clientId is require']
     },
         consumedProduct:{
-            type:Array,
+            type:[consumedProductSchema],
             default:[]
         },
         exerciseDone:{
-            type:Array,
+            type:[exerciseDoneSchema],
             default:[]
         },
         BMR:{
