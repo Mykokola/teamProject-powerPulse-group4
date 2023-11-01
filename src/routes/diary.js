55533075b5effe519033c324d8b301db95e6b7ab
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-11-01 18:46:50 +0200

    прибрав закомічений текст

--- src/routes/diary.js
+++ src/routes/diary.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const controllers = require('../controllers/diary')
-const authMiddleware = require('../middlewears/authMiddleware')
-//PRIVATE
-router.post('/save-product',authMiddleware,controllers.saveProduct)
-router.post('/save-exercise',authMiddleware,controllers.saveExercise)
-router.delete('/delete-product/:productId',authMiddleware,controllers.deleteProduct)
-router.delete('/delete-exercise/:exerciseId',authMiddleware,controllers.deleteExercise)
-router.get('/diary-date-info/:date',authMiddleware,controllers.dairyDateInfo)
-//PRIVATE
+const controllers = require("../controllers/diary");
+const authMiddleware = require("../middlewears/authMiddleware");
+
+router.post("/save-product", authMiddleware, controllers.saveProduct);
+router.post("/save-exercise", authMiddleware, controllers.saveExercise);
+router.delete("/delete-product/:productId", authMiddleware, controllers.deleteProduct);
+router.delete("/delete-exercise/:exerciseId", authMiddleware, controllers.deleteExercise);
+router.get("/diary-date-info/:date", authMiddleware, controllers.dairyDateInfo);
 
 module.exports = router;
--- src/routes/exercises.js
+++ src/routes/exercises.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const ctrl = require("../controllers/exercises");
 const authMiddleware = require("../middlewears/authMiddleware");
 
-//PRIVATE
 router.get("/all-types", authMiddleware, ctrl.getAlldetails);
 
 router.get("/:type", authMiddleware, ctrl.getAllExercises);
 
-//PRIVATE
 module.exports = router;
